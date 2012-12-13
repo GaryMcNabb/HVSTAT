09e165308b394c6e4e8b97c26a9724de3247b16c
greentea039 <lemontea053-eh@yahoo.co.jp>
2012-12-13 13:45:45 +0900

    Fix incorrect changes of previous commit

--- main.js
+++ main.js
@@ -6388,13 +6388,13 @@ function StartBattleAlerts () {
 		element = elements[i];
 		var oldOnClick = element.getAttribute("onclick");
 		var newOnClick = 'if(confirm("Are you sure you want to start this challenge on ' + diff + ' difficulty, with set number: ' + _charss.set + '?\\n';
-		if (_settings.StartAlertHP > HVStat.currHpRate) {
+		if (_settings.StartAlertHP > HVStat.currHpPercent) {
 			newOnClick += '\\n - HP is only '+ HVStat.currHpPercent + '%';
 		}
-		if (_settings.StartAlertMP > HVStat.currMpRate) {
+		if (_settings.StartAlertMP > HVStat.currMpPercent) {
 			newOnClick += '\\n - MP is only '+ HVStat.currMpPercent + '%';
 		}
-		if (_settings.StartAlertSP > HVStat.currSpRate) {
+		if (_settings.StartAlertSP > HVStat.currSpPercent) {
 			newOnClick += '\\n - SP is only '+ HVStat.currSpPercent + '%';
 		}
 		if (_settings.StartAlertDifficulty < _charss.difficulty[0]) {
