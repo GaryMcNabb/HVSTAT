8d3df6172e43ee79f8068099ae482ba17032a5f8
greentea039 <lemontea053-eh@yahoo.co.jp>
2013-06-15 10:09:36 +0900

    Revert "Fix to reuse the style element"
    This reverts commit 66f22f97fc4366f7e414ad61af1bae100d22b59e.

--- hvstat.user.js
+++ hvstat.user.js
@@ -175,12 +175,8 @@ browser.extension.style = {
 			GM_addStyle(styleText);
 		} else {
 			if (!this.element) {
-				this.element = document.querySelector('style[type="text/css"]');
-				if (!this.element) {
-					this.element = document.createElement("style");
-					this.element.type = "text/css";
-					(document.head || document.documentElement).insertBefore(this.element, null);
-				}
+				this.element = document.createElement("style");
+				(document.head || document.documentElement).insertBefore(this.element, null);
 			}
 			this.element.textContent += "\n" + styleText;
 		}
